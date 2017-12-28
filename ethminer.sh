b9f3b4e0d55008bde9e33eb095974ac29973b02e
Pichayoot Ouppaphan <ob@Pichayoots-MacBook.local>
2017-12-28 09:28:55 +0700

    add executable

---
+++ ethminer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/local/bin/ethminer --farm-recheck 200 -U -S eth-us-east1.nanopool.org:9999 -FS eth-us-west1.nanopool.org:9999 -O 0xaa30c433c26ee136d9be7c8b66342a02c3fd5b7d.TeslaV100/pichayoot@gmail.com --cuda-parallel-hash 4
